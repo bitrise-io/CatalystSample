28531ee8537a812bbd5b05fb84ef3776e2ecfff7
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2020-04-09 13:51:49 +0200

    Initial Commit

---
+++ Catalyst Sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Catalyst Sample
+//
+//  Created by Krisztián Gödrei on 2020. 04. 09..
+//  Copyright © 2020. Krisztián Gödrei. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
